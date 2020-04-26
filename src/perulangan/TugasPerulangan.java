f68419aa9591b0277fb284c8d1e4d9aa28f84c45
Fauzan-fti <fauzan.hs20@gmail.com>
2020-04-26 23:48:49 +0800

    Buat clas Penjumlahan

---
+++ src/perulangan/TugasPerulangan.java
@@ -0,0 +1,18 @@
+package perulangan;
+
+import java.util.Scanner;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //Untuk mengimput nilai/data dari user
+
+        System.out.println ("==========|PENJUMLAHAN|==========");
+        System.out.print ("Masukkan Angka = ");
+        int angka = input.nextInt();
+
+        for (int i = 1; i <= 10; ++i)
+            System.out.println (i+". Hasil dari Penjumlahan "+i+" x "+angka+" = "+(i*angka));
+
+    }
+}
+}
