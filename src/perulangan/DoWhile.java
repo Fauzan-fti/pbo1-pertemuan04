92c40934727ecd66c04cbf7226c75161630ad799
Fauzan-fti <fauzan.hs20@gmail.com>
2020-04-26 23:41:36 +0800

    buat class while do

--- src/perulangan/DoWhile.java
+++ src/perulangan/DoWhile.java
@@ -1,6 +1,6 @@
 package perulangan;
 
-public class Main {
+public class DoWhile {
     public static void main(String[] args) {
         int isiGelas = 0;
         int penuh = 225;
@@ -9,12 +9,14 @@ public class Main {
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
 
-        for (isiGelas = 225; isiGelas <= penuh; isiGelas++){
+        do {
+            isiGelas++;
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }
+        }while(isiGelas != penuh)
 
         System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
     }
 
+}
 }
