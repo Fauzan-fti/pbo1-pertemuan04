a76bf50d5d50e41264fe21330f5d2a8d3c97d053
Fauzan-fti <fauzan.hs20@gmail.com>
2020-04-27 00:28:48 +0800

    Buat Class Tugas Array

--- src/array/TugasArray.java
+++ src/array/TugasArray.java
@@ -1,42 +1,29 @@
 package array;
 
+import java.net.SocketOption;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TugasArray {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); //Untuk mengimput nilai/data dari user
 
-        System.out.println("============|KOPERASI UNISKA|============");
-        System.out.print("Masukkan jumlah item nang ditukar = ");
-        int barang    = input.nextInt();
-        String nama[] = new String[barang];
-        int harga[]   = new int[barang];
-        int jumlah[]  = new int[barang];
-        int total[]   = new int[barang];
-        int tot=0;
+        // membuat array Nama Mahasiswa
+        String[] nama = new String[5];
 
-        for (int i=0; i < barang; i++){
-            System.out.print("1. Nama Barang ke-"+(i+1)+" = ");
-            nama[i] = input.next();
-            System.out.print("2. Harga "+nama[i]+" = ");
-            harga[i] = input.nextInt();
-            System.out.print("3. Jumlah "+nama[i]+" = ");
-            jumlah[i] = input.nextInt();
-            total[i] = harga[i]*jumlah[i];
-            System.out.println("4. Total Harga Barang ke-"+(i+1)+" = "+total[i]);
-            System.out.println(" ");
-            tot=tot+total[i];
+        // membuat scanner
+        Scanner scan = new Scanner(System.in);
+
+        // mengisi data ke array
+        for (int i = 0; i < nama.length; i++){
+            System.out.print("Nama Ke- " + i + ":");
+            nama[i] = scan.nextLine();
         }
-        System.out.println("===========|RINCIAN BELANJA|===========");
-        for (int z=0; z < barang; z++){
-            System.out.println("Barang ke-"+(z+1));
-            System.out.println("1. Nama Barang   = "+nama[z]);
-            System.out.println("2. Harga Barang  = "+harga[z]);
-            System.out.println("3. Jumlah Barang = "+jumlah[z]);
-            System.out.println(" ");
+
+        System.out.println("------------------------------");
+
+        // menampilkan semua isi array
+        for (String h : nama) {
+            System.out.println(h);
         }
-        System.out.println("Total Harga Belanja Keseluruhan = "+tot);
-        System.out.println("=============|TARIMA KASIH|=============");
-        System.out.println("Dijual seadanya lah:)");
     }
 }
