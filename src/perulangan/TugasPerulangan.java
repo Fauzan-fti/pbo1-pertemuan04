938d2661f6bc88487d8865f70477170a6eef0ef0
Fauzan-fti <fauzan.hs20@gmail.com>
2020-04-26 23:58:23 +0800

    buat class main

--- src/perulangan/TugasPerulangan.java
+++ src/perulangan/TugasPerulangan.java
@@ -6,13 +6,12 @@ public class TugasPerulangan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //Untuk mengimput nilai/data dari user
 
-        System.out.println ("==========|PENJUMLAHAN|==========");
-        System.out.print ("Masukkan Angka = ");
+        System.out.println ("-----|PENJUMLAHAN|-----");
+        System.out.print ("Masukkan Angka yang ingin di Jumlahkan = ");
         int angka = input.nextInt();
 
         for (int i = 1; i <= 10; ++i)
-            System.out.println (i+". Hasil dari Penjumlahan "+i+" x "+angka+" = "+(i*angka));
+            System.out.println (i+". Hasil dari Penjumlahan "+i+" + "+angka+" = "+(i+angka));
 
     }
-}
 }
